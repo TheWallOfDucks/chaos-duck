644e2c7e2471c91cf784b65ac6ed02526a144441
Caleb Duckwall <caleb.duckwall@apiture.com>
2019-01-08 15:08:00 -0500

    Cleaning

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -2,10 +2,11 @@ import { ECS } from '../classes/ecs';
 
 export const handler = async (event) => {
     const ecs = new ECS();
-    const clusters = await ecs.listClusters();
+    const stoppedTask = await ecs.stopRandomTask();
+    console.log(JSON.stringify(stoppedTask, null, 2));
 
     return {
         statusCode: 200,
-        body: JSON.stringify(clusters, null, 2),
+        body: JSON.stringify(stoppedTask, null, 2),
     };
 };
