2dbaad999a05d46ddee191305fe58585924953c1
Caleb Duckwall <caleb.duckwall@apiture.com>
2019-01-10 09:42:00 -0500

    Adding readme

--- src/classes/utility.ts
+++ src/classes/utility.ts
@@ -1,4 +1,10 @@
 export class Utility {
+    static convertToLowercase(array: string[]) {
+        for (let i = 0; i < array.length; i++) {
+            array[i] = array[i].toLowerCase();
+        }
+        return array;
+    }
     static getRandom(array: any[]) {
         return array[Math.floor(Math.random() * array.length)];
     }
