fac21c52f656b9b3834254d7ce3be912ebb8f85c
Caleb Duckwall <caleb.duckwall@apiture.com>
2019-01-11 16:35:53 -0500

    Modifying CLI to accept config file

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 import * as commander from 'commander';
-const { execSync } = require('child_process');
 
+const { execSync } = require('child_process');
+const fs = require('fs');
 const info = require('../../package.json');
 
-commander
-    .version(info.version, '-v, --version')
-    .description('Chaos Duck')
-    .option('-c, --config <config>', 'If true, will use options defined in json file specified');
+commander.version(info.version, '-v, --version').description('Chaos Duck');
 
 commander
     .command('deploy')
     .alias('d')
+    .option('-c, --config <config>', 'If true, will use options defined in json file specified')
     .option('-e, --environment <environment>', 'Name of AWS environment')
     .option('-a, --account <account>', 'AWS account number')
     .option('-r, --role <role>', 'AWS role')
@@ -20,17 +19,27 @@ commander
     .description('Deploys Chaos Duck based on provided options')
     .allowUnknownOption()
     .action(async (cmd) => {
-        const environment = cmd.environment;
-        const account = cmd.account;
-        const role = cmd.role;
-        const profile = cmd.profile;
-        const stage = cmd.stage;
+        let environment;
+        let account;
+        let role;
+        let profile;
+        let stage;
+        const config = cmd.config;
 
-        console.log('environment => ', environment);
-        console.log('account => ', account);
-        console.log('role => ', role);
-        console.log('profile => ', profile);
-        console.log('stage => ', stage);
+        if (config) {
+            const conf = require(`${process.cwd()}/${config}`);
+            environment = conf.environment;
+            account = conf.account;
+            role = conf.role;
+            profile = conf.profile;
+            stage = conf.stage;
+        } else {
+            environment = cmd.environment;
+            account = cmd.account;
+            role = cmd.role;
+            profile = cmd.profile;
+            stage = cmd.stage;
+        }
 
         const result: Buffer = execSync(`gradle deploy -Daws_env=${environment} -Daws_account=${account} -Daws_role=${role} -Daws_profile=${profile} -Daws_stage=${stage}`);
         console.log(result.toString());
