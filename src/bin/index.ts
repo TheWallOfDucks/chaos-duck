1d34492521e5e93ea8b87331d4d7d20d11c7c83a
Caleb Duckwall <caleb.duckwall@apiture.com>
2019-01-11 17:17:48 -0500

    Adding the ability to invoke chaos duck from cli

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import * as commander from 'commander';
+import axios from 'axios';
 
 const { execSync } = require('child_process');
 const fs = require('fs');
@@ -10,7 +11,7 @@ commander.version(info.version, '-v, --version').description('Chaos Duck');
 commander
     .command('deploy')
     .alias('d')
-    .option('-c, --config <config>', 'If true, will use options defined in json file specified')
+    .option('-c, --config <config>', 'If specified, will use options defined in json file provided')
     .option('-e, --environment <environment>', 'Name of AWS environment')
     .option('-a, --account <account>', 'AWS account number')
     .option('-r, --role <role>', 'AWS role')
@@ -43,6 +44,31 @@ commander
 
         const result: Buffer = execSync(`gradle deploy -Daws_env=${environment} -Daws_account=${account} -Daws_role=${role} -Daws_profile=${profile} -Daws_stage=${stage}`);
         console.log(result.toString());
+    });
+
+commander
+    .command('invoke')
+    .alias('i')
+    .option('-u, --url <url>', 'URL of chaos endpoint')
+    .option('-s, --services <services>', 'Array of services')
+    .description('Invokes chaos lambda')
+    .allowUnknownOption()
+    .action(async (cmd) => {
+        try {
+            const url = cmd.url;
+            const services = cmd.services.split(',');
+
+            const request = axios.post(url, { services });
+            const response = await request;
+
+            console.log(JSON.stringify(response.data, null, 2));
+        } catch (error) {
+            if (error.response) {
+                console.log(JSON.stringify(error.response.data));
+            } else {
+                console.error(error);
+            }
+        }
     });
 
 commander.parse(process.argv);
